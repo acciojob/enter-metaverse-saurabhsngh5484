3975b5bc5f283f09af949855f848a7574c77331a
saurabh singh <86177535+saurabhsngh5484@users.noreply.github.com>
2023-05-25 20:54:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your JS code here. If required.
-zxzsadsbhtjdyhjfyjuxjzfxxczsvdcv
+let btn=document.getElementById("enterBtn");
+let para=document.getElementById("status");
+
+function changePara(){
+	para.innerHTML= "<h1>Entered Metaverse</h1>"
+	
+}
+
+btn.addEventListener("click",changePara)
